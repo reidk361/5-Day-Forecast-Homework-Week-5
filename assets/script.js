dde99f1b9b88676f049d0d60f2fde1053a531ad4
Kalyn Reid <reidkalyn361@gmail.com>
2021-06-13 10:51:48 -0700

    Added 5 Day Forecast API Link.

--- assets/script.js
+++ assets/script.js
@@ -11,8 +11,22 @@ function getCity(city){
   .then(function (data) {
     weatherEl.textContent = (`The weather and temperature in ${data.name} is: ${data.weather[0].description} and ${data.main.temp} F`);
     body.append(weatherEl);
-  })
-}
+    console.log(data);
+    let cityId = data.id;
+        console.log(cityId);
+        getForecast(cityId)
+      })
+    }
+    
+    function getForecast(id){
+        fetch(`https://api.openweathermap.org/data/2.5/forecast?id=${id}&appid=86caed8984442f53f479ce0a825d902d&units=imperial`)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data)
+        })
+      }
 
 function handleSubmit (event){
   event.preventDefault();
