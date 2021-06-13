3b11d4ead9285b18d60094c8415a3acd6042207f
Kalyn Reid <reidkalyn361@gmail.com>
2021-06-13 10:22:04 -0700

    Added input for html and the ability to look up temperature and weather via that input.

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+const body = document.querySelector("body")
+const weatherEl = document.createElement("h1")
+const inputEl = document.getElementById("city-input");
+const submitEl = document.getElementById("submit-button");
+
+function getCity(city){
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=86caed8984442f53f479ce0a825d902d&units=imperial`)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    weatherEl.textContent = (`The weather and temperature in ${data.name} is: ${data.weather[0].description} and ${data.main.temp} F`);
+    body.append(weatherEl);
+  })
+}
+
+function handleSubmit (event){
+  event.preventDefault();
+  getCity(inputEl.value);
+}
+
+submitEl.addEventListener("click", handleSubmit);
