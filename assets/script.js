83591742a843cfc2b4542249cf2d172ef772c75a
Kalyn Reid <reidkalyn361@gmail.com>
2021-06-13 14:10:26 -0700

    Added containers for future weather values and recieved more information for current weather.

--- assets/script.js
+++ assets/script.js
@@ -1,35 +1,61 @@
-const body = document.querySelector("body")
-const weatherEl = document.createElement("h1")
+const header = document.querySelector("header");
+const currentWeather = document.getElementById("current-weather-container");
+const fiveDayWeather = document.getElementById("5day-weather-container");
+const currentWeatherEl = document.createElement("p")
+const prevInput = document.getElementById("prev-input-container");
 const inputEl = document.getElementById("city-input");
 const submitEl = document.getElementById("submit-button");
+const uvContainer = document.getElementById("UV-container");
+const cityNameEl = document.createElement("h2");
+const cityNameContainer = document.getElementById("city-name-container");
 
 function getCity(city){
+  
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=86caed8984442f53f479ce0a825d902d&units=imperial`)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
-    weatherEl.textContent = (`The weather and temperature in ${data.name} is: ${data.weather[0].description} and ${data.main.temp} F`);
-    body.append(weatherEl);
+    cityNameEl.textContent = (`${data.name}, ${data.sys.country}`);
+    cityNameContainer.append(cityNameEl);
     console.log(data);
+    let lat = data.coord.lat;
+    let lon = data.coord.lon;
+    getCurrentForecast(lat,lon);
     let cityId = data.id;
-        console.log(cityId);
-        getForecast(cityId)
-      })
-    }
-    
-    function getForecast(id){
-        fetch(`https://api.openweathermap.org/data/2.5/forecast?id=${id}&appid=86caed8984442f53f479ce0a825d902d&units=imperial`)
-        .then(function (response) {
-          return response.json();
-        })
-        .then(function (data) {
-          console.log(data)
-        })
-      }
+    get5DayForecast(cityId)
+  })
+}
+
+function getCurrentForecast (latitude, longitude) {
+  fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&appid=86caed8984442f53f479ce0a825d902d&units=imperial`)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    currentWeatherEl.innerHTML = (`<h3>Current Weather:</h3><p>${data.current.weather[0].description} <br /> Wind: ${data.current.wind_speed} MPH <br /> Temp: ${data.current.temp} &#730;F <br /> Humidity: ${data.current.humidity}% <br /> <span id = UV-container>UV Index: ${data.daily[data.daily.length-1].uvi}</span> <br /></p>`);
+    currentWeather.append(currentWeatherEl);
+  })
+}
+
+function get5DayForecast(id){
+  fetch(`https://api.openweathermap.org/data/2.5/forecast?id=${id}&appid=86caed8984442f53f479ce0a825d902d&units=imperial`)
+  .then(function (response) {
+     return response.json();
+  })
+  .then(function (data) {
+    console.log(data)
+  })
+}
 
 function handleSubmit (event){
   event.preventDefault();
+  getCity(inputEl.value);
+  inputEl.value = '';
+}
+
+function handleButton (){
   getCity(inputEl.value);
 }
 
